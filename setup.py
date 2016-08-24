17f8fe957c93279a190f840a51712b6c9c016eb5
Nikita Kitaev <nikitakit@gmail.com>
2016-08-23 18:06:53 -0700

    Add basic dependency handling to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 from setuptools import setup, find_packages
+import sys
+
+if sys.version_info < (3, 5):
+    print("Error: xdbg requires Python 3.5 or later")
+    sys.exit(1)
+
 setup(
     name = "xdbg",
     version = "0.1",
     packages = find_packages(),
+    install_requires=[
+        "ipython"
+    ]
 )
