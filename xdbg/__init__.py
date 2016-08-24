e17b13c0d1e72acc9356623ffcf029790de3f768
Nikita Kitaev <nikitakit@gmail.com>
2016-08-23 17:59:52 -0700

    Better error messages if run outside a kernel

--- xdbg/__init__.py
+++ xdbg/__init__.py
@@ -1,4 +1,16 @@
 from .xdbg import Debugger
+
+error = None
+try:
+    get_ipython().kernel
+except NameError:
+    error = "xdbg must be run inside an IPython kernel"
+except AttributeError:
+    error = "xdbg must be run inside an IPython kernel. A running IPython instance was found, but it does not appear to have an associated kernel"
+
+if error is not None:
+    raise ImportError(error)
+
 debugger = Debugger.get_instance()
 
 __all__ = ['debugger']
